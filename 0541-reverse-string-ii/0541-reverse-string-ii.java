3f21dea80a7b03cc59d326c02848aed0da224a55
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-03-27 13:14:37 +0900

    Time: 1 ms (81.2%), Space: 43.6 MB (43.67%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -7,21 +7,51 @@ class Solution {
         // 1. 덩어리당 k만큼 뒤집기 (마지막 덩어리가 k <= x < 2k 더라도)
         // 2. 덩어리당 k만큼 뒤집다가 마지막 덩어리가 k보다 작은 경우에만 전체 뒤집기
 
-        for (int i = 0; i < n; i += 2 * k) {
-            int left = i;
-            // k만큼 뒤집거나, k보다 작은 경우 전체 뒤집기 
-            // 마지막 덩어리에 왔는데, 그게 k보다 작다면 2번 경우의 수로 뒤집어야함
-            int right = Math.min(i + k - 1, n - 1); 
+        // 마지막 덩어리 전까지는 일관된 규칙으로 뒤집어도 됨
+        int beforeLast = s.length()%(2*k) == 0 ? s.length() : s.length() - (2*k);
 
-            while (left < right) {
-            char temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
+        for(int i = 0; i < beforeLast; i+= 2 * k) {
+            int lt = i;
+            int rt = i + k - 1;
+            while(lt < rt) {
+                char tmp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = tmp;
+                lt++;
+                rt--; 
+            }
+        }
+        // 마지막 덩어리 처리
+        // 조건 1 - 마지막 덩어리가 k <= x <2k인 경우 -> 똑같이 처리
+        // 조건 2 - 마지막 덩어리가 k보다 작은 경우엔 전체 뒤집기
+        int size = s.length() % (2 * k);
+        if(s.length()%(2*k) != 0) { // 마지막 덩어리가 남은 경우에만
+            if(size >= k && size < 2*k) {
+            int lt = s.length() - size;
+            int rt = lt + k - 1;
+            while(lt < rt) {
+                char tmp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = tmp;
+                lt++;
+                rt--; 
+            }
+            } else {
+                int lt = s.length() - size;
+                int rt = s.length()-1;
+                     while(lt < rt) {
+                char tmp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = tmp;
+                lt++;
+                rt--; 
+            }
+            }
 
         }
+
+        return new String(arr);
+
+
     }
-     return new String(arr);
-}
 }
