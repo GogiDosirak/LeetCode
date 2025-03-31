390ff9d4f2bc2cde8098be75321f86881a3d27ac
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-01 01:19:36 +0900

    Time: 3 ms (90.65%), Space: 46.5 MB (67.21%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -3,7 +3,6 @@ class Solution {
         // k번만큼 뒤집은 0을 포함하는 슬라이딩 윈도우
         // 0을 만날때마다 count를 ++해주고, k를 넘으면, 왼쪽의 0부분을 하나 지우고 다시 오른쪽 최대범위 탐색
         int count = 0;
-        int curr = 0;
         int answer = 0;
         int lt = 0;
         for(int rt = 0; rt < nums.length; rt++) { //rt를 nums[]의 끝까지 탐색
@@ -13,13 +12,11 @@ class Solution {
 
             while(count > k) {
                 if(nums[lt] == 0) {
-                    curr--;
                     count--;
                 }
                 lt++;
             }
-            curr = rt - lt + 1;
-            answer = Math.max(answer, curr);
+            answer = Math.max(answer, rt - lt + 1);
         }
 
         return answer;
