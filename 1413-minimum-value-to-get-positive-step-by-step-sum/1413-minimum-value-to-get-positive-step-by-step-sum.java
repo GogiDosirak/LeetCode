c0ea91a58ddaf8457c336104f0124f60cade501c
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-01 18:38:31 +0900

    Time: 0 ms (100%), Space: 40.8 MB (90.38%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,19 +1,11 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        // prefix sum을 구하고, 거기서 가장 작은값을 구하자. 
-        // 그럼 걔를 더해줬을 때 (거기까지 진행했을 때) 1이상 이어야 하니까,
-        // (가장 작은값일 때 만족하는 숫자라면, 나머지는 더 큰 값이므로, 더해줘도 무조건 1이상임)
-        // answer + min >= 1 
-        // answer >= 1 - min
-        // asnwer의 최소값은 1 - min이 된다.
-
         int answer = 0;
-        int[] prefix = new int[nums.length];
-        prefix[0] = nums[0];
-        int min = prefix[0];
+        int min = nums[0];
+        int sum = nums[0];
         for(int i = 1; i < nums.length; i++ ) {
-            prefix[i] = prefix[i-1] + nums[i];
-            min = Math.min(min, prefix[i]);
+            sum += nums[i]; // 공간 복잡도 O(1)으로 누적합 구하기
+            min = Math.min(min, sum);
         }
 
         // min값이 양수라면 무조건 1이 정답
