b68365b9fae48a6a25653ede334f2d55ff8f0585
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-15 21:02:41 +0900

    Time: 10 ms (37.01%), Space: 42.8 MB (19.98%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -11,13 +11,6 @@ class Solution {
         int o = map.getOrDefault('o', 0) / 2;
         int n = map.getOrDefault('n', 0);
 
-        // 여기서 가장 작은 숫자만큼 ballon을 만들 수 있다.
-        int min = Integer.MAX_VALUE;
-        min = Math.min(b, a);
-        min = Math.min(min, l);
-        min = Math.min(min, o);
-        min = Math.min(min, n);
-
-        return min;
+         return Math.min(Math.min(Math.min(b, a), Math.min(l, o)), n);
     }
 }
