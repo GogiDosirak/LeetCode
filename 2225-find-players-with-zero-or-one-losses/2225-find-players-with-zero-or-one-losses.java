aea72b1de41b41bd4271c6a9f013374bc0b39581
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-04-16 20:16:20 +0900

    Time: 75 ms (69.22%), Space: 76.8 MB (98.62%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -4,7 +4,7 @@ class Solution {
         List<List<Integer>> answer = new ArrayList<>();
 
         answer.add(new ArrayList<>());
-        answer.add(new ArrayList<>()); // 또는 List<List<Integer>> answer = Arrays.asList(new)
+        answer.add(new ArrayList<>()); // 또는 List<List<Integer>> answer = Arrays.asList(new ArrayList<>(), new ArrayList<>()) - 이 경우 불변리스트이므로 크기를 변경하는 answer.add는 안되지만, 내부 요소를 변경하는 answer.get(0).add는 가능
 
         for(int[] arr : matches) {
             int winner = arr[0];
@@ -14,11 +14,13 @@ class Solution {
             lostMap.put(loser, lostMap.getOrDefault(loser, 0) + 1);
         }
 
-        for(int key : lostMap.keySet()) {
-            if(lostMap.get(key) == 0) {
-                answer.get(0).add(key);
-            } else if(lostMap.get(key) == 1) {
-                answer.get(1).add(key);
+        for(Map.Entry<Integer, Integer> entry : lostMap.entrySet()) {
+            int player = entry.getKey();
+            int lose = entry.getValue();
+            if(lose == 0) {
+                answer.get(0).add(player);
+            } else if(lose == 1) {
+                answer.get(1).add(player);
             }
         }
 
