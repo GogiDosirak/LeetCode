7f1ee01af78dfe77dad487d7639ed17e29644918
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-03 00:23:07 +0900

    Time: 41 ms (66.76%), Space: 63.3 MB (13.75%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -3,23 +3,21 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int answer = -1;
 
-        for(int i = 0; i < nums.length; i++) {
+        for(int n : nums) { // 인덱스가 필요한것이 아님
             int sum = 0;
-            int n = nums[i];
-            while(n > 0) {
-                sum += n % 10;
-                n /= 10;
+            int temp = n;
+            while(temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
             }
 
             if(map.containsKey(sum)) {
-                answer = Math.max(answer, nums[i] + map.get(sum));
+                answer = Math.max(answer, n + map.get(sum));
             }
-            map.put(sum, Math.max(map.getOrDefault(sum, 0),nums[i] ));
+            map.put(sum, Math.max(map.getOrDefault(sum,0), n));
         }
 
         return answer;
-
-
 
         
     }
