38597333b551e941c4e130e15556022925d913f7
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-05-06 15:17:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -3,29 +3,29 @@ class Solution {
         Map<Character, Integer> map1 = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
 
-        int window = s1.length();
-        int lt = 0;
-
         for(char c : s1.toCharArray()) {
             map1.put(c, map1.getOrDefault(c, 0) + 1);
         }
 
-        for(int rt = 0; rt < s2.length(); rt++) {
-            char c = s2.charAt(rt);
+        int window = s1.length();
+
+        for(int i = 0; i < s2.length(); i++) {
+            char c = s2.charAt(i);
             map2.put(c, map2.getOrDefault(c, 0) + 1);
 
-            if(rt - lt + 1 > window) {
-                char target = s2.charAt(lt);
+            if(i >= window) {
+                char target = s2.charAt(i - window);
                 map2.put(target, map2.get(target) - 1);
 
                 if(map2.get(target) == 0) {
                     map2.remove(target);
                 }
-                lt++;
-            }
 
-            if(map1.equals(map2)) return true;
+                if(map1.equals(map2)) {
+                    return true;
+                }
+            }
         }
-        return false; 
+          return false;
     }
 }
