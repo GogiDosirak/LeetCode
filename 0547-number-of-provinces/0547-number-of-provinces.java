bf59bef22b61d0c3c2acef1d97baed3c2d0f059f
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-06-06 19:09:13 +0900

    Time: 0 ms (100%), Space: 48.5 MB (9.31%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,9 +3,9 @@ class Solution {
         boolean[] ch = new boolean[isConnected.length];
         int answer = 0;
         for(int i = 0; i < isConnected.length; i++) {
-            if(ch[i] == false) {
-                DFS(isConnected, ch, i);
+            if(ch[i] != true) {
                 answer++;
+                DFS(isConnected, ch, i);
             }
         }
         return answer;
@@ -14,9 +14,10 @@ class Solution {
     public void DFS(int[][] isConnected, boolean[] ch, int n) {
         ch[n] = true;
         for(int i = 0; i < isConnected.length; i++) {
-            if(ch[i] == false && isConnected[n][i] == 1) {
+            if(ch[i] != true && isConnected[n][i] == 1) {
                 DFS(isConnected, ch, i);
             }
         }
     }
+
 }
