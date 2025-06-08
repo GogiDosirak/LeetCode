8eb6e7423309388bcc790125850023166eb006ad
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-06-08 21:15:36 +0900

    Time: 0 ms (100%), Space: 44.2 MB (87.29%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,17 +1,8 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        List<List<Integer>> graph = new ArrayList<>();
         boolean[] ch = new boolean[rooms.size()];
-        for(int i = 0; i < rooms.size(); i++) {
-            graph.add(new ArrayList<>());
-        }
-        for(int i = 0; i < rooms.size(); i++) {
-            for(int j = 0; j < rooms.get(i).size(); j++) {
-                graph.get(i).add(rooms.get(i).get(j));
-            }
-        }
 
-        DFS(graph, ch, 0);
+        DFS(rooms, ch, 0);
 
         for(boolean answer : ch) {
             if(answer == false) {
@@ -21,11 +12,11 @@ class Solution {
         return true;
     }
 
-    public void DFS(List<List<Integer>> graph, boolean[] ch, int n) {
+    public void DFS(List<List<Integer>> rooms, boolean[] ch, int n) {
         ch[n] = true;
-        for(int i = 0; i < graph.size(); i++) {
-            if(ch[i] != true && graph.get(n).contains(i)) {
-                DFS(graph, ch, i);
+        for(int i = 0; i < rooms.get(n).size(); i++) {
+            if(ch[rooms.get(n).get(i)] != true) {
+                DFS(rooms, ch, rooms.get(n).get(i));
             }
         }
         return;
