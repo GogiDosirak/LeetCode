7084ee78c2b0b019c5111cce5e3bc187e444fe05
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-23 16:59:52 +0900

    Time: 4 ms (46.43%), Space: 54.1 MB (66.97%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+
+            if(Character.isAlphabetic(c)) { 
+                sb.append(c);
+                } else if(c == '#') {
+                    sb.append(sb.toString());
+                } else if(c == '%') {
+                    sb.reverse();
+                } else {
+                   if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                   }
+                }
+        }
+        return sb.toString();
+    }
+}
