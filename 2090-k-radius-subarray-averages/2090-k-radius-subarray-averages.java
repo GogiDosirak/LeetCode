f07e285b50a80ac37e0d61438f0fddeceab5bae2
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-25 16:27:04 +0900

    Time: 4 ms (99.65%), Space: 60.4 MB (95.16%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,33 +1,25 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
+        if(k == 0) {
+            return nums;
+        }
         int[] answer = new int[nums.length];
 
         for(int i = 0; i < nums.length; i++) {
             answer[i] = -1;
         }
 
-        if(2 * k + 1 > nums.length) { // 2 * k + 1, 즉 윈도우 사이즈가 배열의 길이보다 크다면, -1인 배열 반환
-            return answer;
-        } 
-        if(k == 0) { // 0이라면 그냥 첫 배열 반환해주면 됨
-            return nums;
-        }
-
-
-        long sum = 0; // 오버플로우 방지
-        int windowSize = 2 * k + 1;
-        for(int i = 0; i < windowSize; i++) {
+        long sum = 0; // 오버플로우 방지 
+        for(int i = 0; i < nums.length; i++) {
             sum += nums[i];
+            if(i - (k * 2) < 0) {
+                continue;
+            }
+            answer[i - k] = (int) (sum/(k*2+1));
+            
+            // 한 칸 이동 
+            sum-=nums[i-(2*k)];
         }
-        answer[k] = (int) (sum / windowSize); // 첫 평균 저장
-
-        // 첫 윈도우 rt + 1부터 시작
-        for(int i = windowSize; i < nums.length; i++) {
-            sum += nums[i];
-            sum -= nums[i-windowSize];
-            answer[i - k] = (int) (sum / windowSize);
-        }
-
         return answer;
-}
+    }
 }
