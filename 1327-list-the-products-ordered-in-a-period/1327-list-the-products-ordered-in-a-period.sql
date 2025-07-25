bb7b527b0205d32aa36a9a5d16f95c4d1e99aeb2
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-26 01:18:36 +0900

    Time: 827 ms (20.71%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
-SELECT p.product_name, SUM(o.unit) as unit FROM Products p
-INNER JOIN Orders o ON p.product_id = o.product_id
-WHERE DATE_FORMAT(o.order_date, '%Y-%m') = '2020-02' 
-GROUP BY o.product_id, p.product_name
-HAVING SUM(o.unit) >= 100
+SELECT p.product_name, o.unit 
+FROM Products p
+INNER JOIN (SELECT product_id, sum(unit) as unit FROM Orders
+WHERE DATE_FORMAT(order_date,'%Y-%m') = '2020-02'
+GROUP BY product_id
+HAVING SUM(unit) >= 100
+) o 
+ON p.product_id = o.product_id
