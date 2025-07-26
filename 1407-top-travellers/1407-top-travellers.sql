75a79ab15e67c3e2e275346f58a3ed681f7717de
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-27 01:27:30 +0900

    Time: 808 ms (64.78%), Space: 0B (100%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -3,5 +3,5 @@ SELECT u.name, IFNULL(SUM(r.distance), 0) as travelled_distance
 FROM Users u 
 LEFT OUTER JOIN Rides r
 ON u.id = r.user_id
-GROUP BY u.id, u.name
+GROUP BY u.id
 ORDER BY travelled_distance DESC, u.name ASC
