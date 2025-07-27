0f4b3d09e6f950bd1b663a5d89ae086e0b795b23
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-27 11:11:33 +0900

    Time: 5 ms (96.48%), Space: 60.2 MB (97.67%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,36 +1,33 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
-        int[] answer = new int[nums.length];
+     // sliding
+    int lt = 0;
+    int rt = 2*k;
+    
 
-        int lt = 0;
-        int rt = 2*k;
+    if(k == 0) return nums;
 
-        for(int i = 0; i < answer.length; i++) {
-            answer[i] = -1;
-        }
+    int[] arr = new int[nums.length];
+    Arrays.fill(arr, -1);
 
-        if(nums.length - 1 < rt) {
-            return answer;
-        }
-
-        if(k == 0) return nums;
-
-        long sum = 0;
-        for(int i = 0; i <= rt; i++) {
-            sum += nums[i];
-        }
-
-        answer[(lt+rt)/2] = (int)(sum / (rt-lt+1));
+    if(nums.length < rt - lt + 1) return arr;
 
 
-        for(int i = rt + 1; i < nums.length; i++) {
-            sum -= nums[lt];
-            sum += nums[i];
-            lt++;
+    long sum = 0;
+    for(int i = 0; i <= rt; i++) {
+        sum += nums[i];
+    }
+    arr[rt - k] = (int) (sum / (rt - lt + 1));
 
-            answer[(i+lt)/2] = (int)(sum / (i-lt+1)); 
-        }
+    for(int i = k + 1; i < nums.length - k; i++) {
+        sum += nums[++rt];
+        sum -= nums[lt];
+        lt++;
 
-        return answer;    
+        arr[i] = (int) (sum / (rt - lt + 1));
+    }
+
+    return arr;
+     // prefix sum
   }
 }
