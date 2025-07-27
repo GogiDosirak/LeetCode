9aeef2ed740bc6aef4f6ec12792aea3774b3dff8
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-27 10:36:24 +0900

    Time: 0 ms (100%), Space: 41.1 MB (65.78%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,20 +1,19 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        int answer = 0;
-        int min = nums[0];
-        int sum = nums[0];
-        for(int i = 1; i < nums.length; i++ ) {
-            sum += nums[i]; // 공간 복잡도 O(1)으로 누적합 구하기
-            min = Math.min(min, sum);
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i-1] + nums[i];
         }
 
-        // min값이 양수라면 무조건 1이 정답
-        if(min > 0) {
-            answer = 1;
-            return answer;
+        int answer = Integer.MAX_VALUE;
+        for(int n : prefix) {
+            answer = Math.min(n, answer);
         }
 
-        answer = 1 - min ;
-        return answer;
+        if(answer >= 1) return 1;
+
+        return 1 - answer; 
     }
 }
