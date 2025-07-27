cc41719ce57dc90c530b7b42f40541b144e0f5c7
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-28 01:09:50 +0900

    Time: 1 ms (99.68%), Space: 58 MB (79.54%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,21 +1,23 @@
     class Solution {
+
       public int minSubArrayLen(int target, int[] nums) {
-        int answer = Integer.MAX_VALUE;
         int lt = 0;
         int sum = 0;
-        for(int rt = 0; rt < nums.length; rt++) {
-            sum += nums[rt];
-            while(sum >= target) {
-                sum -= nums[lt];
-                lt++;
-                if(sum >= target) {
-                 answer = Math.min(answer, rt-lt+1);
-            }
-            }
-        }
+        int answer = nums.length;
+        boolean flag = false;
 
-        if(answer == Integer.MAX_VALUE) return 0;
-        
-        return answer;
+        for (int rt = 0; rt < nums.length; rt++) {
+          sum += nums[rt];
+          while (sum >= target) { // sum이 크다면 모두 만족하는 subarray
+            answer = Math.min(answer, rt-lt+1);
+            sum -= nums[lt];
+            lt++;
+            flag = true;
+          }
+        }
+        if (!flag) {
+          return 0;
+      }
+       return answer;
     }
- }
+    }
