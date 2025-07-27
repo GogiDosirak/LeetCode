b72083b2cc331a454e0046e1253db63bcc88614f
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-28 01:26:14 +0900

    Time: 11 ms (88.74%), Space: 44.9 MB (30.69%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,32 +1,38 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        char[] arr = s.toCharArray();
-        int answer = 0;
-        int count = 0;
         int lt = 0;
-        // 처음 k 길이 만큼의 subarray에서 모음 개수구하기
-         for(int rt = 0; rt < k; rt++) {
-            if(arr[rt] == 'a' || arr[rt] == 'e' || arr[rt] == 'i' || arr[rt] == 'o' || arr[rt] == 'u') {
+        int rt = k - 1;
+        int answer = Integer.MIN_VALUE;
+
+        int count = 0;
+        for(int i = 0; i < k; i++) {
+            if(isVowel(s.charAt(i))) {
                 count++;
             }
         }
+        answer = Math.max(answer, count);
 
-        answer = count;
-
-        for(int rt = k; rt < s.length(); rt++) {
-             if(arr[rt] == 'a' || arr[rt] == 'e' || arr[rt] == 'i' || arr[rt] == 'o' || arr[rt] == 'u') {
-                count++;
-            }
-            if(arr[lt] == 'a' || arr[lt] == 'e' || arr[lt] == 'i' || arr[lt] == 'o' || arr[lt] == 'u') {
-                count--;
-                lt++;
+        for(int i = rt + 1; i < s.length(); i++) {
+            if(!isVowel(s.charAt(i))) {
+                if(isVowel(s.charAt(lt))) {
+                    count--; 
+                } 
             } else {
-                lt++;
+                if(!isVowel(s.charAt(lt))) {
+                    count++;
+                }
             }
-            answer = Math.max(count, answer);
+            answer = Math.max(answer, count);
+            lt++;
         }
 
         return answer;
-        
+    }
+
+    private boolean isVowel(char c) {
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return true;
+        }
+        return false;
     }
 }
