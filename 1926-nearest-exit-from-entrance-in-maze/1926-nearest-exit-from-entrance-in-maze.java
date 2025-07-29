b923d28073a218977036241206abda55ffe4422c
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-30 01:39:33 +0900

    Time: 4 ms (97.31%), Space: 46.1 MB (36.24%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,10 +1,10 @@
 class Solution {
+    int answer = 0;
     int[] dx = {1,0,-1,0};
     int[] dy = {0,1,0,-1};
+
     public int nearestExit(char[][] maze, int[] entrance) {
         Queue<Node> queue = new LinkedList<>();
-
-        int level = 1;
         queue.add(new Node(entrance[0], entrance[1]));
         maze[entrance[0]][entrance[1]] = '+';
 
@@ -12,19 +12,19 @@ class Solution {
             int size = queue.size();
             for(int i = 0; i < size; i++) {
                 Node node = queue.poll();
+                if((node.x != entrance[0] || node.y != entrance[1]) && (node.x == 0 || node.x == maze.length - 1 || node.y == maze[0].length - 1 || node.y == 0)) {
+                    return answer;
+                }
                 for(int j = 0; j < 4; j++) {
                     int nx = node.x + dx[j];
                     int ny = node.y + dy[j];
                     if(nx >= 0 && nx < maze.length && ny >= 0 && ny < maze[0].length && maze[nx][ny] == '.') {
-                        if(nx == 0 || nx == maze.length - 1 || ny == 0 || ny == maze[0].length - 1) {
-                            return level;
-                        }
+                        queue.add(new Node(nx,ny));
                         maze[nx][ny] = '+';
-                        queue.add(new Node(nx, ny));
                     }
                 }
             }
-            level++;
+            answer++;
         }
         return -1;
     }
