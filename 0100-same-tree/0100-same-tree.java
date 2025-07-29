6b0d9a03659b9f68f7900221ee31d77621e8ffd7
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-29 23:48:44 +0900

    Time: 0 ms (100%), Space: 41.3 MB (40.71%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,22 +15,23 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        return DFS(p,q);
+    }
+
+    public boolean DFS(TreeNode p, TreeNode q) {
         if(p == null && q == null) {
             return true;
         }
-
         if(p == null || q == null) {
             return false;
         }
-
         if(p.val != q.val) {
             return false;
         }
 
+        boolean a = DFS(p.left, q.left);
+        boolean b = DFS(p.right, q.right);
 
-        boolean left = isSameTree(p.left, q.left);
-        boolean right = isSameTree(p.right, q.right);
-
-        return left && right;
+        return a && b;
     }
 }
