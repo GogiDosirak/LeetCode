7c6f53190d12143028ee62731646663ac81d4fad
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-07-29 14:51:06 +0900

    Time: 13 ms (76.35%), Space: 48.9 MB (34.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        int[] answer = new int[k];
 
         for(int n : nums) {
-            map.put(n, map.getOrDefault(n,0) + 1);
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
 
-        for(int n : map.keySet()) {
-            list.add(n);
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+
+        for(int key : map.keySet()) {
+            heap.add(key);
         }
 
-        list.sort((a,b) -> map.get(b) - map.get(a));
 
+        int[] answer = new int[k];
         for(int i = 0; i < k; i++) {
-            answer[i] = list.get(i);
+            answer[i] = heap.poll();
         }
 
         return answer;
