c8368e6a0b0e744158f9e193a61f75f1421f66f7
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-09-02 12:10:23 +0900

    Time: 1211 ms (5.01%), Space: 45.7 MB (5.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,25 @@
 class Solution {
+    int[] answer = new int[2];
+    List<Integer> list = new ArrayList<>();
     public int[] twoSum(int[] nums, int target) {
-        // 완탐으로 풀어보기
-        int n = nums.length;
-        int[] answer = new int[2];
-        for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++ ) {
-                int sum = nums[i] + nums[j];
-                if(sum == target) {
-                    answer[0] = i;
-                    answer[1] = j;
-                    break;
-                }
-            }
-        }
+        // 백트래킹
+        backtracking(nums, 0, target);
         return answer;
+    }
+
+    public void backtracking(int[] nums, int start, int target) {
+        if(list.size() == 2) {
+            if(nums[list.get(0)] + nums[list.get(1)] == target) {
+                answer[0] = list.get(0);
+                answer[1] = list.get(1);
+            }
+            return; // 깊이 2 (list.size() == 2) 까지 내려오면 더 이상 탐색할 필요가 없다.
+        }
+
+        for(int i = start; i < nums.length; i++) {
+            list.add(i);
+            backtracking(nums, i + 1, target);
+            list.remove(list.size() - 1);
+        }
     }
 }
