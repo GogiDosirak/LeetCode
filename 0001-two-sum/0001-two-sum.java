6d1219ffeaf491024a5ac0a683785c3cffacac12
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-09-02 11:01:04 +0900

    Time: 34 ms (47.59%), Space: 44.9 MB (79.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, List<Integer>> map = new HashMap<>(); //값, 인덱스
+        // 완탐으로 풀어보기
+        int n = nums.length;
         int[] answer = new int[2];
-
-        for(int i = 0; i < nums.length; i++) {
-            map.put(nums[i], new ArrayList<Integer>());
-        }
-
-        for(int i = 0; i < nums.length; i++) {
-            map.get(nums[i]).add(i);
-        }
-
-        for(int key : map.keySet()) {
-            int k = target - key;
-            if(map.containsKey(k)) {
-                if(map.get(k).size() >= 2) {
-                    answer[0] = map.get(k).get(0);
-                    answer[1] = map.get(k).get(1);
-                } else {
-                    answer[0] = map.get(key).get(0);
-                    answer[1] = map.get(k).get(0);
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++ ) {
+                int sum = nums[i] + nums[j];
+                if(sum == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                    break;
                 }
             }
         }
-
         return answer;
-        
     }
 }
