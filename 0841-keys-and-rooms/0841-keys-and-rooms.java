5ddf73ddd3dce0dfc311b5134941a00b3b841bf3
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-09-10 20:57:34 +0900

    Time: 0 ms (100%), Space: 44.1 MB (94.88%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,24 +1,22 @@
 class Solution {
+    boolean[] visited;
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean[] ch = new boolean[rooms.size()];
-
-        DFS(rooms, ch, 0);
-
-        for(boolean answer : ch) {
-            if(answer == false) {
-                return false;
-            }
+        visited = new boolean[rooms.size()];
+        DFS(rooms, 0);
+        for(boolean b : visited) {
+            if(b == false) return false;
         }
+
         return true;
     }
 
-    public void DFS(List<List<Integer>> rooms, boolean[] ch, int n) {
-        ch[n] = true;
-        for(int i = 0; i < rooms.get(n).size(); i++) {
-            if(ch[rooms.get(n).get(i)] != true) {
-                DFS(rooms, ch, rooms.get(n).get(i));
+    public void DFS(List<List<Integer>> rooms, int room) {
+        List<Integer> list = rooms.get(room);
+        visited[room] = true;
+        for(int i = 0; i < list.size(); i++) {
+            if(visited[list.get(i)] == false) {
+                DFS(rooms, list.get(i));
             }
         }
-        return;
     }
 }
