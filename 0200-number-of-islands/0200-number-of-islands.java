b3e340d2cac9109c8ee90fb3c1e58ed4f8c78e54
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-09-16 02:18:33 +0900

    Time: 3 ms (84.03%), Space: 49.4 MB (81.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,28 +1,27 @@
 class Solution {
-    int answer = 0;
     int[] dx = {1,0,-1,0};
     int[] dy = {0,1,0,-1};
     public int numIslands(char[][] grid) {
-        Queue<int[]> queue = new LinkedList<>();
+        int answer = 0;
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == '1') {
                     answer++;
-                    queue.offer(new int[]{i,j});
-                    while(!queue.isEmpty()) {
-                        int[] cord = queue.poll();
-                        for(int h = 0; h < 4; h++) {
-                            int nx = cord[0] + dx[h];
-                            int ny = cord[1] + dy[h];
-                            if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == '1') {
-                                grid[nx][ny] = '0';
-                                queue.offer(new int[]{nx,ny});
-                            }
-                        }
-                    }
+                    DFS(grid, i, j);
                 }
             }
         }
         return answer;
+    }
+
+    public void DFS(char[][] grid, int x, int y) {
+        grid[x][y] = '0';
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == '1') {
+                DFS(grid, nx, ny);
+            }
+        }
     }
 }
