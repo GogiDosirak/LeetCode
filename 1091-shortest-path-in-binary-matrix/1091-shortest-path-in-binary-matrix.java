97bbaa2fa7e13953fb25ea9b1424bddeaee352ac
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-09-16 02:50:39 +0900

    Time: 12 ms (95.43%), Space: 45.6 MB (52.99%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,46 +1,42 @@
 class Solution {
     int[] dx = {1,1,0,-1,-1,-1,0,1};
     int[] dy = {0,1,1,1,0,-1,-1,-1};
+    int answer = 1;
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid[0][0] == 1) {
-            return -1;
-        }
+        BFS(grid, new Node(0,0));
+        return answer;
+    }
 
-        if(grid.length == 1) {
-            return 1;
-        }
-
+    public void BFS(int[][] grid, Node start) {
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0,0));
-        grid[0][0] = 1;
-        int level = 1;
-
+        if(grid[start.x][start.y] == 0) {
+            queue.add(start);
+            }
         while(!queue.isEmpty()) {
             int size = queue.size();
             for(int i = 0; i < size; i++) {
-                Node node = queue.poll();
+                Node now = queue.poll();
+                if(now.x == grid.length - 1 && now.y == grid[0].length - 1) return;
                 for(int j = 0; j < 8; j++) {
-                    int nx = node.x + dx[j];
-                    int ny = node.y + dy[j];
+                    int nx = now.x + dx[j];
+                    int ny = now.y + dy[j];
                     if(nx >= 0 && nx < grid.length && ny >= 0 && ny < grid[0].length && grid[nx][ny] == 0) {
-                        if(nx == grid.length - 1 && ny == grid[0].length -1) {
-                            return level + 1;
-                        }
-                        queue.add(new Node(nx,ny));
                         grid[nx][ny] = 1;
+                        queue.add(new Node(nx,ny));
                     }
                 }
             }
-            level++;
+            answer++;
         }
-        return -1;
-    }
 
+        answer = -1;
+    }
 }
 
 class Node {
     int x;
     int y;
+
     public Node(int x, int y) {
         this.x = x;
         this.y = y;
