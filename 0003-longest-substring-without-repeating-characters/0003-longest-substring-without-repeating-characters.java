a6d1d935da3d6cb908319cae21da9ae4f5d209fc
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-10-07 01:08:27 +0900

    Time: 8 ms (18.22%), Space: 45.1 MB (17.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,20 +1,29 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // 중복된 문자가 존재하지 않는 가장 긴 substring 구하기
+        // 0 <= s.length <= 5 * 10^4   ->   O(N^2) 불가능
+        // 영어, 숫자, 공백, 특수문자로 이루어짐
+
+        // 투포인터 -> O(N)
+        // 오른쪽으로 하나씩 늘려가다가, 2인 얘가 나오면 1이 될 때 까지 왼쪽으로 당김 
+        
+        Map<Character, Integer> map = new HashMap<>();
         int lt = 0;
         int answer = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        
-
         for(int rt = 0; rt < s.length(); rt++) {
-            map.put(s.charAt(rt), map.getOrDefault(s.charAt(rt), 0) + 1);
-            while(map.get(s.charAt(rt)) > 1) {
-                map.put(s.charAt(lt), map.get(s.charAt(lt)) - 1);
+            char c = s.charAt(rt);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            while(map.get(c) >= 2) {
+                char ltC = s.charAt(lt);
+                map.put(ltC, map.get(ltC) - 1);
+                if(map.get(ltC) == 0) {
+                    map.remove(ltC);
+                }
                 lt++;
             }
             answer = Math.max(answer, rt - lt + 1);
         }
 
         return answer;
-        
     }
 }
