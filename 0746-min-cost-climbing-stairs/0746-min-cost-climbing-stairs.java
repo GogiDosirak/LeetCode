71be649ca23dc31ca3867d1e620001d4c98a3531
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-10-16 14:56:04 +0900

    Time: 0 ms (100%), Space: 43.5 MB (28.71%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,13 +1,20 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
+        // 최소로 가는법 -> n-2 또는 n-1을 최소로 갱신 -> 거기서 점프하는게 현재 칸까지 올 수 있는 최소 비용
+        // n칸 도달 -> n-1 또는 n-2까지 올 수 있는 최소비용 중에 최소비용
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+
         dp[0] = cost[0];
         dp[1] = cost[1];
 
-        for(int i = 2; i < cost.length; i++) {
-            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+        for(int i = 2; i <= n; i++) {
+            if(i == n) {
+                dp[i] = Math.min(dp[i-1], dp[i-2]);
+            } else {
+                dp[i] = Math.min(dp[i-1],dp[i-2]) + cost[i];
+            }
         }
-
-        return Math.min(dp[cost.length-1], dp[cost.length-2]);
+        return dp[n];
     }
 }
