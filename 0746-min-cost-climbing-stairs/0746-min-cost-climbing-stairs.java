7bdc343e3fd9f49e49cd873047097909b918be0d
GogiDosirak <154963143+GogiDosirak@users.noreply.github.com>
2025-10-16 15:12:17 +0900

    Time: 2 ms (5.4%), Space: 44.7 MB (5.58%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,20 +1,25 @@
 class Solution {
+    int[] dp = new int[1001]; 
     public int minCostClimbingStairs(int[] cost) {
-        // 최소로 가는법 -> n-2 또는 n-1을 최소로 갱신 -> 거기서 점프하는게 현재 칸까지 올 수 있는 최소 비용
-        // n칸 도달 -> n-1 또는 n-2까지 올 수 있는 최소비용 중에 최소비용
-        int n = cost.length;
-        int[] dp = new int[n + 1];
-
+        // cost[i]가 0일수도 있음
+        Arrays.fill(dp,-1);
+        
         dp[0] = cost[0];
         dp[1] = cost[1];
 
-        for(int i = 2; i <= n; i++) {
-            if(i == n) {
-                dp[i] = Math.min(dp[i-1], dp[i-2]);
-            } else {
-                dp[i] = Math.min(dp[i-1],dp[i-2]) + cost[i];
-            }
+        
+        return dynamic(cost.length, cost);
+    }
+
+    public int dynamic(int n, int[] cost) {
+        if (n == 0) return cost[0];
+        if (n == 1) return cost[1];
+        if (dp[n] != -1) return dp[n];
+
+        if(n == cost.length) {
+            return dp[n] = Math.min(dynamic(n-2, cost), dynamic(n-1, cost));
+        } else {
+            return dp[n] = Math.min(dynamic(n-2, cost), dynamic(n-1, cost)) + cost[n];
         }
-        return dp[n];
     }
 }
